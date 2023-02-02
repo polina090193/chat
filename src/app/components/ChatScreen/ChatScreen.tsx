58d6ac66cfd44dc534421d727aef15c95e105b70
Polina Guseva <polina_0901@mail.ru>
2023-02-02 18:00:04 +0300

    Make other btns

--- src/app/components/ChatScreen/ChatScreen.tsx
+++ src/app/components/ChatScreen/ChatScreen.tsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import Pusher from 'pusher-js';
 import * as keys from '@chat/keys'
 import { Message, MessageWithID } from '@chat/types'
+import Button from '../Button/Button';
 
 const StyledChatScreen = styled.div`
   // Your style here
@@ -18,10 +19,10 @@ export function ChatScreen() {
 
   const getMessages = () => {
     axios.get('http://localhost:3333/api/messages')
-        .then(res => {
+      .then(res => {
           /* TODO if (res.data.status === 'success')  */setMessages(res.data)
-        })
-        .catch(e => console.log(e))
+      })
+      .catch(e => console.log(e))
   }
 
   useEffect(() => {
@@ -44,19 +45,20 @@ export function ChatScreen() {
 
   const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>): void => setMsgText(e.target.value)
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+  const handleKeyDownMessage = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === 'Enter' && msgText.length) sendMessage()
+  }
+  const clickSendMessageBtn = (): void => sendMessage()
 
-    if (e.key === 'Enter' && msgText.length) {
+  const sendMessage = () => {
+    const payload = {
+      username: userInfo.username,
+      message: msgText
+    };
+    axios.post('http://localhost:3333/api/messages', payload)
+      .catch(e => console.log(e))
 
-      const payload = {
-        username: userInfo.username,
-        message: msgText
-      };
-      axios.post('http://localhost:3333/api/messages', payload)
-        .catch(e => console.log(e))
-
-      setMsgText('')
-    }
+    setMsgText('')
   }
 
   const handleLogout = (e: React.MouseEvent): void => {
@@ -66,9 +68,9 @@ export function ChatScreen() {
   return (
     <StyledChatScreen data-testid="chat-screen" >
       <h2>Hello, {userInfo.username}</h2>
-      <button type="button" onClick={handleLogout}>Logout</button>
+      <Button buttonText="Logout" onClickAction={handleLogout} />
       <div>
-        { messages.map(chat => <div key={chat.id}><strong>{chat.username}</strong>: {chat.message}</div>) }
+        {messages.map(chat => <div key={chat.id}><strong>{chat.username}</strong>: {chat.message}</div>)}
       </div>
       <input
         type="text"
@@ -77,9 +79,10 @@ export function ChatScreen() {
         placeholder="chat here..."
         className="form-control"
         onChange={handleTextChange}
-        onKeyDown={handleKeyDown}
+        onKeyDown={handleKeyDownMessage}
       />
-      {/* TODO send button */}
+      <Button buttonText="Send" onClickAction={clickSendMessageBtn} />
+
     </StyledChatScreen>
   );
 }
